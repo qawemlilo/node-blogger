f188b8b6c3943a2b04e21ab5441f41174086566d
Raging Flame <qawemlilo@gmail.com>
2013-05-09 20:29:42 +0200

    ...

---
+++ config.js
@@ -0,0 +1,35 @@
+/*
+    This is the config file that contains the blog configuration data.
+    
+    Note: Please replace the data with your details.
+*/
+module.exports = {
+    blog: {
+        name: 'Raging Flame Blog',
+        description: 'Rants about code and stuff...',
+        keywords: ['Joomla! Extensions', 'JavaScript', 'Node.js', 'PHP'],
+        baseUrl: '/',
+        postsFolder: __dirname + '/posts',
+        postsFile: './posts.json',
+        url: 'http://localhost:3080',
+        rss: '/rss',
+        rssLimit: 10
+    },
+    
+    author: {
+        name: 'Qawelesizwe Mlilo',
+        autobio: 'Hi, my name is Qawelesizwe Mlilo(or Que), I write code and build stuff on the web. I\'m at my happiest when programming in JavaScript but writing custom Joomla! extensions in PHP pays my bills.',
+        email: 'qawemlilo@gmail.com',
+        twitterHandle: 'ragingflameblog',
+        website: 'http://www.ragingflame.co.za',
+        facbookPage: '#',
+        googlePage: 'https://plus.google.com/111595084798587457827/posts?hl=en&partnerid=gplp0',
+        avatar: '/img/qawe.png'
+    },
+    
+    showDate: true,
+    
+    showPagination: true,
+    
+    showShareButtons: true
+};
