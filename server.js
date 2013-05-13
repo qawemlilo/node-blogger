1382571d25d3b4e7d652a36d5597ee65ce0764fc
Raging Flame <qawemlilo@gmail.com>
2013-05-13 20:39:38 +0200

    ...

--- server.js
+++ server.js
@@ -8,10 +8,11 @@ var http = require('http'),
     port = process.env.PORT || 3080,
     routes = require('./routes'), 
     app,
-    OneDay = (1000 * 60 * 60 * 24);
+    OneDay = 86400000;
 
 app = connect()
   .use(connect.compress())
+  .use(connect.static(__dirname + '/template'))
   .use(connect.static(__dirname + '/template'), { maxAge: OneDay })
   .use(routes);
 
